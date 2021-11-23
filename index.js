00b62457a25c4bc0caeb89e1f8cb47efcfd2fa4b
Luis Azocar <lazocar.dev@gmail.com>
2021-11-23 19:14:10 -0300

    Adding Express

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 8080;
 
-http
-  .createServer((req, res) => {
-    res.setHeader("Content-Disposition", "attachment; filename=lista.csv");
-    res.writeHead(200, {
-      "Content-Type": "application/csv",
-    });
+// Server Static Content
+app.use(express.static("public"));
 
-    // const person = {
-    //   id: 1,
-    //   name: "Luis Azocar",
-    // };
+// app.get("/", (req, res) => {
+//   res.send("Home Page");
+// });
 
-    res.write("id, name");
-    res.write("1, Fernando");
-    res.write("2, Maria");
-    res.write("3, Jose");
-    res.write("4, Pedro");
-    res.end();
-  })
-  .listen(8080);
+app.get("/hola-mundo", (req, res) => {
+  res.send("Hello World");
+});
 
-console.log("Escuchando en el puerto", 8080);
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/404.html");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
