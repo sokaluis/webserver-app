bb3f296b4a493126deaa01bc407d0bfa84bc1f7a
Luis Azocar <lazocar.dev@gmail.com>
2021-11-24 19:49:39 -0300

    Applying Handlebars

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.set("view engine", "hbs");
 // Server Static Content
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   res.send("Home Page");
-// });
+app.get("/", (req, res) => {
+  res.render("home", {
+    name: "Luis Azocar",
+    title: "Curso de Node",
+  });
+});
 
 app.get("/generic", (req, res) => {
   res.sendFile(__dirname + "/public/generic.html");
