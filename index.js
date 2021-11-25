5be48c9247481c8f3a45d5334ed48a37d204ee58
Luis Azocar <lazocar.dev@gmail.com>
2021-11-24 21:47:57 -0300

    Angular Build

--- index.js
+++ index.js
@@ -5,36 +5,36 @@ const hbs = require("hbs");
 const app = express();
 const port = process.env.PORT;
 
-app.set("view engine", "hbs");
+// app.set("view engine", "hbs");
 
-hbs.registerPartials(__dirname + "/views/partials");
+// hbs.registerPartials(__dirname + "/views/partials");
 
 // Server Static Content
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home", {
-    name: "Luis Azocar",
-    title: "Curso de Node",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.render("home", {
+//     name: "Luis Azocar",
+//     title: "Curso de Node",
+//   });
+// });
 
-app.get("/generic", (req, res) => {
-  res.render("generic", {
-    name: "Luis Azocar",
-    title: "Curso de Node",
-  });
-});
+// app.get("/generic", (req, res) => {
+//   res.render("generic", {
+//     name: "Luis Azocar",
+//     title: "Curso de Node",
+//   });
+// });
 
-app.get("/elements", (req, res) => {
-  res.render("elements", {
-    name: "Luis Azocar",
-    title: "Curso de Node",
-  });
-});
+// app.get("/elements", (req, res) => {
+//   res.render("elements", {
+//     name: "Luis Azocar",
+//     title: "Curso de Node",
+//   });
+// });
 
 app.get("*", (req, res) => {
-  res.render("404");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
